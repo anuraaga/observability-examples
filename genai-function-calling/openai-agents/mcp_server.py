5ee9c3689a7083bcd50aa069305a6e3074659c4f
Anuraag Agrawal <anuraaga@gmail.com>
2025-04-10 16:39:26 +0900

    line length

--- genai-function-calling/openai-agents/mcp_server.py
+++ genai-function-calling/openai-agents/mcp_server.py
@@ -2,6 +2,7 @@ import asyncio
 import contextlib
 import httpx
 from mcp.server.fastmcp import FastMCP
+from opentelemetry.instrumentation.starlette import StarletteInstrumentor
 import uvicorn
 
 
@@ -10,12 +11,17 @@ async def mcp_server(tools):
     mcp_server = FastMCP(log_level="WARNING")
     for tool in tools:
         mcp_server.add_tool(tool)
+    app = mcp_server.sse_app()
+    # Import order issues seem to affect starlette auto instrumentation so
+    # manually instrument for now.
+    StarletteInstrumentor.instrument_app(app)
+
     # Manually setup uvicorn to allow shutting it down
     config = uvicorn.Config(
-        mcp_server.sse_app(),
+        app,
         host="localhost",
         port=8000,
-        log_level="critical",  # To suppress an SSE background task cancellation ERROR
+        log_level="critical",
         timeout_graceful_shutdown=1,
     )
     server = uvicorn.Server(config)
